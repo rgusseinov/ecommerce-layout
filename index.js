3cc24771b693fbf021c84f3d7e1b382e9060061d
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-06-26 21:16:01 +0600

    feat: finish gateway layout

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector(".menu-catalog__link").onclick = function (e) {
+/* document.querySelector(".menu-catalog__link").onclick = function (e) {
   const overlay = document.querySelector(".header-overlay");
   const submenu = document.querySelector(".menu-catalog__link");
 
@@ -7,3 +7,13 @@ document.querySelector(".menu-catalog__link").onclick = function (e) {
 
   e.preventDefault();
 };
+ */
+
+const toggler = document.getElementsByClassName("caret");
+
+for (let i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function () {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
